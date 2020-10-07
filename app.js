8ab5412aa35b0abea167fe6edd2402ee787470ec
Kasey <mail.kaseyc@gmail.com>
2020-10-07 08:01:41 -0500

    first API call

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//Using "request" node module
+//Using request node module
+const request = require('request');
+
+const url = "http://api.weatherstack.com/current?access_key=44bbcac212311578107f1f0200340d3c&query=41.521910,-93.626250"
+
+request({url: url}, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log((data.current));
+});
